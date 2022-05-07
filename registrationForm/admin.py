6c9bdc6736bdb8e33ec6ef4a8ec75f3840911bca
shashidharyellenki <yellenkishashidhar@msitprogram.net>
2022-05-07 12:22:25 +0530

    created models files

--- registrationForm/admin.py
+++ registrationForm/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from . models import newUser
+# registering the model with dashboard
+admin.site.register(newUser)
--- registrationForm/models.py
+++ registrationForm/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class newUser(models.Model):
+    # personal details
+    firstName = models.CharField(max_length=100, blank=False)
+    lastName = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(max_length=100, unique=True, blank=False)
+    profile_photo = models.ImageField()
+    shortBio=models.TextField(max_length=200, blank=False)
+    tags = models.TextField(max_length=100, blank=True)
+    # Acdamic details
+    schoolName = models.CharField(max_length=100, blank=False)
+    schoolBoard = models.CharField(max_length=100, blank=False)
+    Graduation = models.CharField(max_length=100, blank=False)
+    GraduationUniversity = models.CharField(max_length=100, blank=False)
+    postUniversity = models.CharField(max_length=100, blank=False)
+    postUniversity = models.CharField(max_length=100, blank=False)
+    # social handles
+    Linkedin = models.CharField(max_length=100, blank=False)
+    facebook = models.CharField(max_length=100, blank=False)
+    Github = models.CharField(max_length=100, blank=False)
+    Instagram = models.CharField(max_length=100, blank=False)
+    # Work Exp
+    Position = models.CharField(max_length=100, blank=False)
+    org = models.CharField(max_length=100, blank=False)
+    From = models.DateField(auto_now=False)
+    to = models.DateField(auto_now=False)
+
+    # display
+
+    def __str__(self):
+        return self.firstName
+    
