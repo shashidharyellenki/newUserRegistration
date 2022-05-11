d28db3e3530d03e1c4da64d6c6770d8ee2cfce45
shashidharyellenki <yellenkishashidhar@msitprogram.net>
2022-05-11 23:30:33 +0530

    added loggings to prod

--- Forms/settings/prod.py
+++ Forms/settings/prod.py
@@ -11,3 +11,34 @@ ALLOWED_HOSTS=[
 
     'user-registrationstest.herukuapp.com',
 ]
+DEBUG_PROPAGATE_EXCEPTIONS = True
+LOGGING = {
+'version': 1,
+'disable_existing_loggers': False,
+'formatters': {
+'verbose': {
+'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+'datefmt' : "%d/%b/%Y %H:%M:%S"
+},
+'simple': {
+'format': '%(levelname)s %(message)s'
+},
+},
+'handlers':{
+'console': {
+'level': 'DEBUG',
+'class': 'logging.StreamHandler',
+},
+},
+'loggers': {
+'MYAPP': {
+'handlers': ['console'],
+'level': 'DEBUG',
+},
+}
+}
+# Heroku settings
+
+django_on_heroku.settings(locals(),staticfiles=False)
+
+del DATABASES['default']['OPTIONS']['sslmode']
