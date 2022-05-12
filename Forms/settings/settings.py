b956afa0c5333755d86b8eed10244da8a5098731
shashidharyellenki <yellenkishashidhar@msitprogram.net>
2022-05-12 08:51:10 +0530

    added whitenoise

--- Forms/settings/settings.py
+++ Forms/settings/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL= 'static/'
 STATICFILES_DIRS=[
