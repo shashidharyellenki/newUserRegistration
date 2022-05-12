2d2b27483c38b8947f585a99d7ab9982fc1ea82a
shashidharyellenki <yellenkishashidhar@msitprogram.net>
2022-05-12 08:36:03 +0530

    chnaged urls

--- Forms/urls.py
+++ Forms/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('', include('Registrations.urls')),
     path('admin/', admin.site.urls),
 ]
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATICFILES_DIRS[0])
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
