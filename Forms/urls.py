79b256a4e5112937d7abcd9e5f2bf779b1a82127
shashidharyellenki <yellenkishashidhar@msitprogram.net>
2022-05-20 18:15:28 +0530

    path admin

--- Forms/urls.py
+++ Forms/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('', include('Registrations.urls')),
-    path('admin/', admin.site.urls),
+    path(r'^admin/', admin.site.urls),
 ]
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATICFILES_DIRS[0])
 
